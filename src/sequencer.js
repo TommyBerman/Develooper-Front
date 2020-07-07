75aeb5e60ab342b71f71667f7699ffc407115bee
AdShEaTaKoRe <tomy.berman1@gmail.com>
2020-07-07 12:44:16 +0100

    last commit before staging

--- src/sequencer.js
+++ src/sequencer.js
@@ -1,104 +1,95 @@
-// const baseURL =
-//   process.env.NODE_ENV === "production"
-//     ? "https://develooper.herokuapp.com"
-//     : "http://localhost:3000";
+const compositionURL = "http://127.0.0.1:3000/compositions/"
 
-
-
-const compositionURL = `${baseURL}/compositions`;
-const soundsURL = `${baseURL}/sounds`;
 
 function saveComposition(composition, cb) {
-  fetch(compositionURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({ composition: composition }),
-  })
-    .then((resp) => resp.json())
-    .then(cb);
+    fetch(compositionURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({composition: composition})
+    })
+    .then(resp => resp.json())
+    .then(cb)
 }
 
 function getUserCompositions(userId, cb) {
-  fetch(compositionURL + "list/" + userId, {
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  })
-    .then((resp) => resp.json())
-    .then(cb);
+    fetch(compositionURL + 'list/' + userId, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    .then(resp => resp.json())
+    .then(cb)
 }
+
+
 
 //////////////////////////////////
 
 function sequencer() {
-  const kick = new Tone.Player(`${soundsURL}/Tr8 Kick 04`).toDestination();
-  const snare = new Tone.Player(`${soundsURL}/snare1`).toDestination();
-  const hiHat = new Tone.Player(`${soundsURL}/hi-hat1`).toDestination();
-  const tom = new Tone.Player(`${soundsURL}/tom-analog`).toDestination();
-  const ride = new Tone.Player(`${soundsURL}/ride-acoustic02`).toDestination();
-  const crash = new Tone.Player(`${soundsURL}/crash-tape`).toDestination();
 
-  let index = 0;
 
-  Tone.Transport.scheduleRepeat(repeat, "16n");
+    const kick = new Tone.Player("http://127.0.0.1:3000/sounds/kick-acoustic01").toDestination()
+    const snare = new Tone.Player("http://127.0.0.1:3000/sounds/snare1").toDestination()
+    const hiHat = new Tone.Player("http://127.0.0.1:3000/sounds/hi-hat1").toDestination()
+    const tom = new Tone.Player("http://127.0.0.1:3000/sounds/tom-analog").toDestination()
+    const ride = new Tone.Player("http://127.0.0.1:3000/sounds/ride-acoustic02").toDestination()
+    const crash = new Tone.Player("http://127.0.0.1:3000/sounds/crash-tape").toDestination()
 
-  const startStopButton = document.querySelector("#startStop");
 
-  startStopButton.addEventListener("click", () => {
-    if (startStopButton.checked) {
-      Tone.Transport.start();
-    } else {
-      Tone.Transport.stop();
+    let index = 0
+
+    Tone.Transport.scheduleRepeat(repeat, "16n")
+
+    const startStopButton = document.querySelector('#startStop')
+
+    startStopButton.addEventListener('click', () => {
+        if (startStopButton.checked) {
+            Tone.Transport.start()
+        } else {
+            Tone.Transport.stop()
+        }
     }
-  });
+    )
 
-  function repeat() {
-    let step = index % 16;
 
-    let kickInputs = document.querySelector(
-      `.kick input:nth-child(${step + 1})`
-    );
-    if (kickInputs.checked) {
-      kick.start();
-    }
+    function repeat() {
+        let step = index % 16
 
-    let snareInputs = document.querySelector(
-      `.snare input:nth-child(${step + 1})`
-    );
-    if (snareInputs.checked) {
-      snare.start();
-    }
+        let kickInputs = document.querySelector(`.kick input:nth-child(${step + 1})`)
+        if (kickInputs.checked) {
+            kick.start()
+        }
 
-    let rideInputs = document.querySelector(
-      `.ride input:nth-child(${step + 1})`
-    );
-    if (rideInputs.checked) {
-      ride.start();
-    }
+        let snareInputs = document.querySelector(`.snare input:nth-child(${step + 1})`)
+        if (snareInputs.checked) {
+            snare.start()
+        }
 
-    let hiHatInputs = document.querySelector(
-      `.hi-hat input:nth-child(${step + 1})`
-    );
-    if (hiHatInputs.checked) {
-      hiHat.start();
-    }
+        let rideInputs = document.querySelector(`.ride input:nth-child(${step + 1})`)
+        if (rideInputs.checked) {
+            ride.start()
+        }
 
-    let tomInputs = document.querySelector(`.tom input:nth-child(${step + 1})`);
-    if (tomInputs.checked) {
-      tom.start();
-    }
+        let hiHatInputs = document.querySelector(`.hi-hat input:nth-child(${step + 1})`)
+        if (hiHatInputs.checked) {
+            hiHat.start()
+        }
 
-    let crashInput = document.querySelector(
-      `.crash input:nth-child(${step + 1})`
-    );
-    if (crashInput.checked) {
-      crash.start();
+        let tomInputs = document.querySelector(`.tom input:nth-child(${step + 1})`)
+        if (tomInputs.checked) {
+            tom.start()
+        }
+
+        let crashInput = document.querySelector(`.crash input:nth-child(${step + 1})`)
+        if (crashInput.checked) {
+            crash.start()
+        }
+
+        index++
     }
 
-    index++;
-  }
 }
--- src/users.js
+++ src/users.js
@@ -1,32 +1,30 @@
-const baseURL =
-  //   process.env.NODE_ENV === "production"
-  //     ? "https://develooper.herokuapp.com"
-  //     : "http://localhost:3000";
-  "https://develooper.herokuapp.com";
-const userURL = `${baseURL}/users`;
+const userURL = "http://127.0.0.1:3000/users/"
+
 
 function newUser(user, cb) {
-  fetch(userURL, {
+    fetch(userURL, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
-      Headers: "application/json",
+        "Content-Type": "application/json",
+        "Headers": "application/json"
     },
-    body: JSON.stringify({ user: user }),
-  })
-    .then((resp) => resp.json())
-    .then(cb);
+    body: JSON.stringify({user: user})
+    })
+    .then(resp => resp.json())
+    .then(cb)
+    
 }
 
 function loginUser(user, cb) {
-  fetch(userURL + "login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Headers: "application/json",
-    },
-    body: JSON.stringify({ user: user }),
-  })
-    .then((resp) => resp.json())
-    .then(cb);
+    fetch(userURL + 'login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Headers": "application/json"
+        },
+        body: JSON.stringify({user: user})
+    })
+    .then(resp => resp.json())
+    .then(cb)
+    
 }
